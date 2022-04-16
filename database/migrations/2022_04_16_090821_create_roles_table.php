5a6d6199218005a22534534555acb733a528d1dd
level237 <Bramslevel129@gmail.com>
2022-04-16 11:00:51 +0100

    Relationship OneToMany: Relationship between User and Role tables

--- database/migrations/2022_04_16_090821_create_roles_table.php
+++ database/migrations/2022_04_16_090821_create_roles_table.php
@@ -13,15 +13,10 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('roles', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('lastname');
-            $table->integer('phone');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('description');
             $table->timestamps();
         });
     }
@@ -33,6 +28,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('roles');
     }
 };
