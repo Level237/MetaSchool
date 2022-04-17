d764dad1723818e2cd84af494b4f6751057468f2
level237 <Bramslevel129@gmail.com>
2022-04-17 19:53:28 +0100

    relation morphToMany:Relation between topicable pivot table and school table

--- app/Models/topic.php
+++ app/Models/topic.php
@@ -4,9 +4,15 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\MorphToMany;
+use App\Models\school;
 
 class topic extends Model
 {
     use HasFactory;
 
+    public function schools():MorphToMany
+    {
+        return $this->morphToMany(school::class,'topicable');
+    }
 }
