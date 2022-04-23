ba4e694f1601be1eac13fabd6dcf98b3da4a641e
level237 <Bramslevel129@gmail.com>
2022-04-23 13:36:00 +0100

    header init:header content

---
+++ resources/views/layouts/frontend/header.blade.php
@@ -0,0 +1,65 @@
+<!-- header -->
+<header id="site-header" class="fixed-top">
+    <div class="container">
+        <nav class="navbar navbar-expand-lg navbar-light">
+            <a class="navbar-brand" href="index.html"><i class="fas fa-graduation-cap"></i>MetaSchool
+            </a>
+            <button class="navbar-toggler collapsed" type="button" data-bs-toggle="collapse"
+                data-bs-target="#navbarScroll" aria-controls="navbarScroll" aria-expanded="false"
+                aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon fa icon-expand fa-bars"></span>
+                <span class="navbar-toggler-icon fa icon-close fa-times"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarScroll">
+                <ul class="navbar-nav ms-auto my-2 my-lg-0 navbar-nav-scroll">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="index.html">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="courses.html">Courses</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="contact.html">Contact</a>
+                    </li>
+                </ul>
+                <!-- search-right -->
+                <div class="header-search position-relative">
+                    <div class="search-right mx-lg-2">
+                        <a href="#search" class="btn search-btn p-0" title="search">
+                            <i class="fas fa-search"></i></a>
+                        <!-- search popup -->
+                        <div id="search" class="pop-overlay">
+                            <div class="popup">
+                                <form action="#search" method="GET" class="search-box">
+                                    <input type="search" placeholder="Enter Keyword..." name="search"
+                                        required="required" autofocus="">
+                                    <button type="submit" class="btn"><span class="fas fa-search"
+                                            aria-hidden="true"></span></button>
+                                </form>
+                            </div>
+                            <a class="close" href="#close">×</a>
+                        </div>
+                        <!-- //search popup -->
+                    </div>
+                </div>
+                <!--//search-right-->
+            </div>
+            <!-- toggle switch for light and dark theme -->
+            <div class="cont-ser-position">
+                <nav class="navigation">
+                    <div class="theme-switch-wrapper">
+                        <label class="theme-switch" for="checkbox">
+                            <input type="checkbox" id="checkbox">
+                            <div class="mode-container">
+                                <i class="gg-sun"></i>
+                                <i class="gg-moon"></i>
+                            </div>
+                        </label>
+                    </div>
+                </nav>
+            </div>
+            <!-- //toggle switch for light and dark theme -->
+        </nav>
+    </div>
+</header>
+<!-- //header -->
