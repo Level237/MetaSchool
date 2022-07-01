e472ded624beed8d22f953ae5f498396a86596d2
level237 <Bramslevel129@gmail.com>
2022-07-01 17:52:03 +0200

    seed update

--- database/seeders/AdminSeeder.php
+++ database/seeders/AdminSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use App\Models\User;
+use Illuminate\Support\Str;
 
 class AdminSeeder extends Seeder
 {
@@ -17,6 +18,8 @@ class AdminSeeder extends Seeder
     {
         User::create([
             'name' => 'Metashool',
+            'lastname'=>'Meta',
+            'phone'=>67890307,
             'email' => 'Metashool@gmail.com',
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,7 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-use Database\Seeders\AdminSeeder;
+
 
 class DatabaseSeeder extends Seeder
 {
