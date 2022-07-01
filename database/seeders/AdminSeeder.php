d2a870cbee774407756c69d9a67dc56a8ecfece7
level237 <Bramslevel129@gmail.com>
2022-07-01 17:32:13 +0200

    Seeder Admin

--- database/seeders/AdminSeeder.php
+++ database/seeders/AdminSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\User;
 
 class AdminSeeder extends Seeder
 {
@@ -14,6 +15,13 @@ class AdminSeeder extends Seeder
      */
     public function run()
     {
-        //
+        User::create([
+            'name' => 'Metashool',
+            'email' => 'Metashool@gmail.com',
+            'email_verified_at' => now(),
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'remember_token' => Str::random(10),
+            'isAdmin'=>1
+        ]);
     }
 }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Database\Seeders\AdminSeeder;
 
 class DatabaseSeeder extends Seeder
 {
@@ -15,5 +16,6 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+        $this->call(AdminSeeder::class);
     }
 }
